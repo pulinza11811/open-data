3894cca876f5ecff082fc47fd3dc4c6f20fbb845
pulinza11811 <103770001+pulinza11811@users.noreply.github.com>
2024-05-07 17:10:59 +0700

    update

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const connectDB = async () =>
     try
     {
         //await mongoose.connect('mongodb://127.0.0.1:27017/information')
-        await mongoose.connect('mongodb+srv://pulinza11811:VbFHoNbi48tL0ZHo@province-name.hj16yzc.mongodb.net/?retryWrites=true&w=majority&appName=province-name')
+        await mongoose.connect('mongodb+srv://pulinza11811:VbFHoNbi48tL0ZHo@province-name.hj16yzc.mongodb.net/?retryWrites=true&w=majority&appName=province-name/test')
+        //await mongoose.connect(process.env.MONGODB_CONNECT_URI)
         console.log('DB Connected');
     }
     catch (err)
