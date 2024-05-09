f7c4d13eee36fd33b91bddcd01faf4b1e41f8935
pulinza11811 <103770001+pulinza11811@users.noreply.github.com>
2024-05-09 08:20:20 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>
     try
     {
         //await mongoose.connect('mongodb://127.0.0.1:27017/information')
-        await mongoose.connect('mongodb+srv://pulinza11811:VbFHoNbi48tL0ZHo@province-name.hj16yzc.mongodb.net/?retryWrites=true&w=majority&appName=province-name/test')
+        await mongoose.connect('mongodb+srv://pulinza11811:XShgZKpTEbTxEGyQ@province-name.hj16yzc.mongodb.net/?retryWrites=true&w=majority&appName=province-name')
         //await mongoose.connect(process.env.MONGODB_CONNECT_URI)
         console.log('DB Connected');
     }
